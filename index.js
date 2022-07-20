b534f8675cd3795ed62f5f60c89aa44dfeb19c44
Parker Parham <parkerparham@gmail.com>
2022-07-20 12:37:05 -0400

    Changed original index.js to index.js so I could redo lab. Assignment completed in new index.js file.

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-// Write your solution in this file!
 const employee = {
-    name: "Jim",
-    streetAddress: "11 Mellow Way",
+    name: "Lisa",
+    streetAddress: "11 Mark Way"
 }
-function updateEmployeeWithKeyAndValue(obj, key, value) {
-    const newEmployee = {...obj};
-    newEmployee[key] = value;
-    return newEmployee
+function updateEmployeeWithKeyAndValue(employeeObject, key, value){
+    return {
+        ...employeeObject,
+        [key]: value,
+    }
 }
-
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
     employee[key] = value;
     return employee
 }
-function deleteFromEmployeeByKey(employee, key) {
+function deleteFromEmployeeByKey(employee, key,){
     const differentEmployee = {...employee}
     delete differentEmployee[key]
     return differentEmployee
 }
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key] 
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
     return employee
 }
